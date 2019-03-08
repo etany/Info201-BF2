02aa9e1909e162140ec9ac82c623e8d993872b6a
uw <uw@D-10-19-98-26.dhcp4.washington.edu>
2019-03-07 16:26:24 -0800

    Create UI file and make templates for each tab

---
+++ mcd_project/ui.R
@@ -0,0 +1,102 @@
+# McDonald's Nutritional Data UI
+# Group BF2 Final Project
+
+library(shiny)
+library(dplyr)
+library(ggplot2)
+
+# Define UI for application that draws a histogram
+shinyUI(
+  fluidPage(
+
+    # Tab page one
+    tabPanel(
+      # Tab label
+      "",
+      # Application title
+      titlePanel("panelTitle"),
+
+      # Sidebar
+      sidebarLayout(
+        sidebarPanel(
+          # Sidebar code goes here
+        ),
+
+        # Shows graphic in main panel of tab
+        mainPanel(
+           plotOutput("firstPlot")
+        )
+      )
+    ),
+
+    # Tab page two
+    tabPanel(
+      # Tab label
+      "",
+      # Application title
+      titlePanel("panelTitle"),
+
+      # Sidebar
+      sidebarLayout(
+        sidebarPanel(
+          # Sidebar code goes here
+        ),
+
+        # Shows graphic in main panel of tab
+        mainPanel(
+          plotOutput("secondPlot")
+        )
+      )
+    ),
+
+    # Tab page three
+    tabPanel(
+      # Tab label
+      "",
+      # Application title
+      titlePanel("panelTitle"),
+
+      # Sidebar
+      sidebarLayout(
+        sidebarPanel(
+          # Sidebar code goes here
+        ),
+
+        # Shows graphic in main panel of tab
+        mainPanel(
+          plotOutput("thirdPlot")
+        )
+      )
+    ),
+
+    # Tab page four
+    tabPanel(
+      # Tab label
+      "",
+      # Application title
+      titlePanel("panelTitle"),
+
+      # Sidebar
+      sidebarLayout(
+        sidebarPanel(
+          # Sidebar code goes here
+        ),
+
+        # Shows graphic in main panel of tab
+        mainPanel(
+          plotOutput("fourthPlot")
+        )
+      )
+    ),
+
+    # Tab page five: about tab
+    tabPanel(
+      # Tab label
+      "about",
+      # Application title
+      titlePanel("About"),
+      hr(),
+      p(em("This project is in the process of being built"))
+    )
+  )
+)
