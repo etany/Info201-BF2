815ad4e32f5db8862d4879d07489bdf2fcef9558
JL <ljf123@uw.edu>
2019-03-11 18:50:51 -0700

    worked on branch graph1

--- mcd_project/server.R
+++ mcd_project/server.R
@@ -4,6 +4,7 @@
 library(shiny)
 library(dplyr)
 library(ggplot2)
+library(plotly)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
@@ -11,8 +12,15 @@ shinyServer(function(input, output) {
   # Read in McDonald's data
   mcd_df   <- read.csv("./data/mcd35.csv", stringsAsFactors = F)
 
-  output$firstPlot <- renderPlot({
+  output$firstPlot <- renderPlotly({
     # Make a function in a separate file in scripts folder and call it here.
+    selected <- mcd_df %>% filter(CATEGORY == as.character(input$cat_var))
+    plot <- plot_ly(selected, x = ~ (get(input$x_var)), y = ~ITEM, 
+                    type = "scatter") %>% 
+      layout(xaxis = list(title = "", tickfont = list(size = 5)), 
+             yaxis = list(title = "", tickfont = list(size = 5)), 
+             height = 600)
+    plot
   })
 
   output$secondPlot <- renderPlot({
--- mcd_project/ui.R
+++ mcd_project/ui.R
@@ -4,29 +4,70 @@
 library(shiny)
 library(dplyr)
 library(ggplot2)
+library(plotly)
 
 # Define UI for application that draws a histogram
 shinyUI(
-  fluidPage( theme = shinytheme("united"),
+  fluidPage(theme = shinytheme("united"),
 
     # Tab page one
     tabPanel(
       # Tab label
-      "",
+      "Nutrition for all items",
       # Application title
-      titlePanel("panelTitle"),
+      titlePanel("Nutrition for all items"),
 
       # Sidebar
       sidebarLayout(
         sidebarPanel(
           # Sidebar code goes here
+          selectInput(
+            "x_var",
+            label = "Select a nutrition",
+            choices = list(
+              "Calories" = "CAL", 
+              "Fat" = "FAT",
+              "Saturated Fat" = "SFAT",
+              "Trans Fat" = "TFAT",
+              "Chloride" = "CHOL",
+              "Salt" = "SALT",
+              "Carbon" = "CARB",
+              "Fiber" = "FBR",
+              "Sugar" = "SGR",
+              "Protein" = "PRO"
+            )
+          ),
+          
+          selectInput(
+            "cat_var",
+            label = "Select a category",
+            choices = list(
+              "Burger" = "BURGER", 
+              "Chicken" = "CHICKEN",
+              "Breakfast" = "BREAKFAST",
+              "Salad" = "SALAD",
+              "Snackside" = "SNACKSIDE",
+              "Beverage" = "BEVERAGE",
+              "McCafe" = "MCCAFE",
+              "Dessert" = "DESSERT",
+              "Condiment" = "CONDIMENT",
+              "All Day Breakfast" = "ALLDAYBREAKFAST",
+              "Biscuit" = "ADBISCUIT",
+              "Muffin" = "ADBMUFFIN",
+              "Happy Meal" = "HAPPYMEAL",
+              "Signature" = "SIGNATURE",
+              "McPick 2A" = "MCPICK2A",
+              "McPick 2B" = "MCPICK2B",
+              "McPick 2C" = "MCPICK2C"
+            )
+          )
         ),
 
         # Shows graphic in main panel of tab
         mainPanel(
-           plotOutput("firstPlot")
+           plotlyOutput("firstPlot"), width = 12, height = 20
         )
-      )
+      ), position = "right", style='width: 800px; height: 900px'
     ),
 
     # Tab page two
