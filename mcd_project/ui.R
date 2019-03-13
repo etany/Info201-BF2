524d4153611c9cf341df37829760d6893be99ccb
kyryllkeydanskyy <kyryll.k@gmail.com>
2019-03-13 16:23:23 -0700

    Move contents for plot two into main UI, add units tocolumn names and rename varaibles respectively

--- mcd_project/ui.R
+++ mcd_project/ui.R
@@ -21,9 +21,21 @@ mcd_df <- mcd_df %>%
          "Sodium (mg)" = "SALT",
          "Carbohydrates (g)" = "CARB",
          "Fiber (g)" = "FBR",
-         "Sugar  (g)" = "SGR",
+         "Sugar (g)" = "SGR",
          "Protein (g)" = "PRO",
          "Category" = "CATEGORY")
+
+dv_nutr_cats <- c("Calories" = "dvCAL",
+                  "Fat" = "dvFAT",
+                  "Saturated Fat" = "dvSFAT",
+                  "Cholesterol" = "dvCHOL",
+                  "Sodium" = "dvSALT",
+                  "Carbohydrates" = "dvCARB",
+                  "Fiber" = "dvFBR",
+                  "Protein" = "dvPRO"
+)
+
+all_menu_items <- unique(mcd_df$"Item")
 
 # Define UI
 shinyUI(navbarPage(
@@ -48,15 +60,15 @@ shinyUI(navbarPage(
             label = "Select a nutrition",
             choices = list(
               "Calories",
-              "Fat",
-              "Saturated Fat",
-              "Trans Fat",
-              "Cholesterol",
-              "Salt",
-              "Carbohydrates",
-              "Fiber",
-              "Sugar",
-              "Protein"
+              "Fat (g)",
+              "Saturated Fat (g)",
+              "Trans Fat (g)",
+              "Cholesterol (mg)",
+              "Sodium (mg)",
+              "Carbohydrates (g)",
+              "Fiber (g)",
+              "Sugar (g)",
+              "Protein (g)"
             )
           ),
 
@@ -95,110 +107,127 @@ shinyUI(navbarPage(
     # Tab page two
     tabPanel(
       # Tab label
-      "tabTwo",
+      "Daily Values %",
       # Application title
-      titlePanel("panelTitle"),
-
-      # Sidebar
-      sidebarLayout(
-        sidebarPanel(
-          # Sidebar code goes here
-        ),
-
-        # Shows graphic in main panel of tab
-        mainPanel(
-          plotOutput("secondPlot")
-        )
-      )
-    ),
-
-    # Page three plot nutrition by total fat
-    tabPanel(
-      # Tab label
-      "Total Fat vs. Nutrition",
-      # Add a titlePanel to your tab
       titlePanel("Select Data"),
-
-      # Create a sidebar layout for this tab (page)
-      sidebarLayout(
-
-        # Create a sidebarPanel for your controls
-        sidebarPanel(
-
-          # Make a sliderInput widget for looking at a specified total fat range
-          sliderInput(inputId = "totalfat",
-                      label = "Select Total Fat Range",
-                      min = min(mcd_df$Fat),
-                      max = max(mcd_df$Fat),
-                      value = c(min(mcd_df$Fat),
-                                max(mcd_df$Fat)
-                                )
-                      ),
-
-          # Add dropdown to select nutrition variable for x axis
-          selectInput(
-            inputId = "x_nutr_fat",
-            label = "Choose Nutrition Variable to Plot Against Total Fat",
-            choices = list(
-              "Total Saturated Fats" = "Saturated Fat",
-              "Total Trans Fats" = "Trans Fat",
-              "Total Cholesterol" = "Cholesterol",
-              "Total Salt" = "Salt",
-              "Total Carbohydrates" = "Carbohydrates",
-              "Total Fiber" = "Fiber",
-              "Total Sugar" = "Sugar",
-              "Total Protein" = "Protein"
-            )
-          )
-        ),
-
-        # Display scatter plot on main panel
-        mainPanel(
-          plotOutput("scatter")
-        )
-      )
-    ),
-
-    # Tab page four
-    tabPanel(
-      # Tab label
-      "tabFour",
-      # Application title
-      titlePanel("panelTitle"),
-
       # Sidebar
       sidebarLayout(
         sidebarPanel(
-          # Sidebar code goes here
+          selectInput("item_selected",
+                      label = "Food Item",
+                      choices = all_menu_items),
+          selectizeInput("cat_selected",
+                         label = "Nutritional Categories",
+                         choices = dv_nutr_cats,
+                         multiple = T,
+                         selected = dv_nutr_cats[1])
         ),
-
         # Shows graphic in main panel of tab
         mainPanel(
-          plotOutput("fourthPlot")
+          h3("Comparison Between the Nutritional Value of
+                      McDonalds Items and the Suggested Daily Value*"),
+          plotlyOutput("dv_plot_2"),
+          p(em("*As Specified by the FDA Based on a 2,000 Calorie
+                        Intake for Adults and Children 4 or More Years of Age.",
+               a("Source",
+                 href = "https://www.fda.gov/ICECI/Inspections/
+                          InspectionGuides/ucm114098.htm#ATTACHMENT_8"))),
+          p("This plot serves to show what portion of the total daily
+                     nutritional value each single item from the McDonalds menu
+                     satisfies. If an item’s particular nutritional category
+                     exceeds the suggested daily intake, then it is
+                     highlighted in red. ")
         )
       )
-    ),
-
-    # Tab page five: about tab
-    tabPanel(
-      # Tab label
-      "about",
-      # Application title
-      titlePanel("About us"),
-      hr(),
-      h2("This project is brought to you by a group of Univeristy of Washington students enrolled in Info201."),
-      h3(" Our team members include: "),
-      h4("Jiafei Li"),
-      h6( "Jiafei is a senior student pursuing Sociolgy and minoring in Infomatics." ),
-      h4("Erica Tan"),
-      h6( "Erica is a senior student pursuing Sociolgy and minoring in Infomatics."),
-      h4("Khalifa Al-Maslamani"),
-      h6(""),
-      h4("Kyryll Keydansky"),
-      h6(""),
-      h4("Igor Podgorny"),
-      h6(""),
-      p(em("This project is in the process of being built"))
     )
-))
+)
+    # # Page three plot nutrition by total fat
+    # tabPanel(
+    #   # Tab label
+    #   "Total Fat vs. Nutrition",
+    #   # Add a titlePanel to your tab
+    #   titlePanel("Select Data"),
+    # 
+    #   # Create a sidebar layout for this tab (page)
+    #   sidebarLayout(
+    # 
+    #     # Create a sidebarPanel for your controls
+    #     sidebarPanel(
+    # 
+    #       # Make a sliderInput widget for looking at a specified total fat range
+    #       sliderInput(inputId = "totalfat",
+    #                   label = "Select Total Fat Range",
+    #                   min = min(mcd_df$"Fat (g)"),
+    #                   max = max(mcd_df$"Fat (g)"),
+    #                   value = c(min(mcd_df$"Fat (g)"),
+    #                             max(mcd_df$"Fat (g)")
+    #                             )
+    #                   ),
+    # 
+    #       # Add dropdown to select nutrition variable for x axis
+    #       selectInput(
+    #         inputId = "x_nutr_fat",
+    #         label = "Choose Nutrition Variable to Plot Against Total Fat",
+    #         choices = list(
+    #           "Total Saturated Fats" = "Saturated Fat (g)",
+    #           "Total Trans Fats" = "Trans Fat (g)",
+    #           "Total Cholesterol" = "Cholesterol (mg)",
+    #           "Total Salt" = "Sodium (mg)",
+    #           "Total Carbohydrates" = "Carbohydrates (g)",
+    #           "Total Fiber" = "Fiber (g)",
+    #           "Total Sugar" = "Sugar (g)",
+    #           "Total Protein" = "Protein (g)"
+    #         )
+    #       )
+    #     ),
+    # 
+    #     # Display scatter plot on main panel
+    #     mainPanel(
+    #       plotOutput("scatter")
+    #     )
+    #   )
+    # ),
+
+    # # Tab page four
+    # tabPanel(
+    #   # Tab label
+    #   "tabFour",
+    #   # Application title
+    #   titlePanel("panelTitle"),
+    # 
+    #   # Sidebar
+    #   sidebarLayout(
+    #     sidebarPanel(
+    #       # Sidebar code goes here
+    #     ),
+    # 
+    #     # Shows graphic in main panel of tab
+    #     mainPanel(
+    #       plotOutput("fourthPlot")
+    #     )
+    #   )
+    # ),
+
+    # # Tab page five: about tab
+    # tabPanel(
+    #   # Tab label
+    #   "about",
+    #   # Application title
+    #   titlePanel("About us"),
+    #   hr(),
+    #   h2("This project is brought to you by a group of Univeristy of Washington students enrolled in Info201."),
+    #   h3(" Our team members include: "),
+    #   h4("Jiafei Li"),
+    #   h6( "Jiafei is a senior student pursuing Sociolgy and minoring in Infomatics." ),
+    #   h4("Erica Tan"),
+    #   h6( "Erica is a senior student pursuing Sociolgy and minoring in Infomatics."),
+    #   h4("Khalifa Al-Maslamani"),
+    #   h6(""),
+    #   h4("Kyryll Keydansky"),
+    #   h6(""),
+    #   h4("Igor Podgorny"),
+    #   h6(""),
+    #   p(em("This project is in the process of being built"))
+    # )
+)
 
