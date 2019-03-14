a587291a7ec6d8a20393d035b57819c5cfd7687f
etany <38017405+etany@users.noreply.github.com>
2019-03-14 14:55:49 -0700

    new

--- mcd_project/page_4_Erica/app.R
+++ mcd_project/page_4_Erica/app.R
@@ -2,7 +2,11 @@ library(dplyr)
 library(shiny)
 library(DT)
 
-data<-read.csv("../data/mcd35.csv")
+data<-read.csv("./data/mcd35.csv")
 category <- data$CATEGORY
+cal <- select(data,CAL)
+cal_range <-range(data$CAL)
+fat <- select(data, FAT)
+fat_range <- range(data$FAT)
 
 shinyApp(ui = ui, server = server)
